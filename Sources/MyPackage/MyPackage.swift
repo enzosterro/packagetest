0f091e996fefe774b7a2d0b9bf26c4acf9068e86
Enzo Sterro <enzo.sterro@gmail.com>
2020-06-26 14:11:41 +0300

    Use UIKit version

--- Sources/MyPackage/MyPackage.swift
+++ Sources/MyPackage/MyPackage.swift
@@ -1,19 +1,32 @@
-import SwiftUI
+//import SwiftUI
+//
+//public struct MyImageView: View {
+//
+//    public var body: some View {
+//        Image("Image", bundle: Bundle.module)
+//            .resizable()
+//            .scaledToFit()
+//    }
+//
+//    public init() {}
+//
+//}
+//
+//struct MyPackage_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MyImageView()
+//    }
+//}
 
-public struct MyImageView: View {
+import UIKit
 
-    public var body: some View {
-        Image("Image", bundle: Bundle.module)
-            .resizable()
-            .scaledToFit()
+public struct ImageView {
+
+    public let image: UIImage?
+
+    public init() {
+        image = UIImage(named: "Image", in: Bundle.module, compatibleWith: nil)
     }
-
-    public init() {}
 
 }
 
-struct MyPackage_Previews: PreviewProvider {
-    static var previews: some View {
-        MyImageView()
-    }
-}
